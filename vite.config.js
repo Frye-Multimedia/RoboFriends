294459eedd9defb4d62b8c87091db9d62717791c
Daniel <Daniel.Frye1983@gmail.com>
2025-05-16 14:14:08 -0500

    vite config js file base name fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/RoboFridnds/',
+  base: '/RoboFriends/',
   plugins: [
     react(),
     tailwindcss()
