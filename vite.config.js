710f5965330935301c04f482f73dacbae8907d37
Daniel <Daniel.Frye1983@gmail.com>
2025-05-16 13:53:38 -0500

    GitHig Pages Setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-react-tailwind/',
+  base: '/RoboFridnds/',
   plugins: [
     react(),
     tailwindcss()
